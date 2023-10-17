8bf5e622b71b8033b5ae784d61cc4ac79c53ab0b
h3x4d3cim4l <przeglowa@gmail.com>
2023-10-17 23:10:50 +0200

    UI: Basic styles 0.0.01

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{html,ts}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'custom-navbar': 'rgb(76, 17, 178)',
+        'navbar-hover': '#5616c4',
+        'navbar-hover-text': '#0f2a54',
+      }
+    },
   },
   plugins: [],
 }
