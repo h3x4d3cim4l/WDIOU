be397c697b227f14da2bd7c9cecccc022eb98805
h3x4d3cim4l <przeglowa@gmail.com>
2024-01-04 18:50:19 +0100

    islogged basic logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(){
+
+  }
+  isLogged():boolean{
+    return false;
+  }
   title = 'WDIOU';
 }
